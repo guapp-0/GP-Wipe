3a51571eaf2840931e07ec8676c7ba7d7544da24
guapp0 <121726488+guapp-0@users.noreply.github.com>
2025-05-23 23:07:00 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,10 @@ author 'guapp0'
 description 'A simple script to manage player wipes'
 version '1.0.0'
 
-
-server_scripts {
+shared_script 'config.lua'
+server_script {
     '@oxmysql/lib/MySQL.lua',
-    '@ox_lib/init.lua',
-    'server.lua',
-    'exports.lua'
+    'server.lua'
 }
 
-dependencies {
-    'oxmysql',
-    'ox_lib',
-    'es_extended'
-}
+dependency 'oxmysql'
